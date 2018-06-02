f6b69ac3ad7125ca45e28f1a74972691ef56c776
rahul <rahulpattanshetty@gmail.com>
2018-06-02 18:43:31 +0530

    created dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'dashboard/index'
   devise_for :users
+  root to: 'dashboard#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
